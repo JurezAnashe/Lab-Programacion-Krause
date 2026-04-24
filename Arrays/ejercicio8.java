99ae4cac9b436d6a611828d0679a289b3bbd9870
Juarez asheee <lucasjuar2008@gmail.com>
2026-04-24 20:32:34 -0300

    Create ejercicio8.java

---
+++ Arrays/ejercicio8.java
@@ -0,0 +1,71 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ejercicio8 {
+    public static void main(String[] args) {
+        String[] palabras = { "benedetto", "pavon", "cristiano", "messi", "maradona", "pele", "ronaldo", "zidane" };
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        String palabra = palabras[random.nextInt(palabras.length)];
+        char[] descubierta = new char[palabra.length()];
+
+        for (int i = 0; i < palabra.length(); i++) {
+            descubierta[i] = '_';
+        }
+
+        int errores = 0;
+        int erroresmax = 9;
+
+        System.out.println("adivina la palabra");
+        System.out.println("tenes " + errores + " intentos");
+
+        while (errores < erroresmax) {
+            System.out.print("palabra: ");
+            for (int i = 0; i < descubierta.length; i++) {
+                System.out.print(descubierta[i] + " ");
+            }
+            System.out.println("  (" + errores + " intentos)");
+
+            boolean gano = true;
+            for (int i = 0; i < descubierta.length; i++) {
+                if (descubierta[i] == '_') {
+                    gano = false;
+                    break;
+                }
+            }
+
+            if (gano) {
+                System.out.println("\nadivinaste la palabra: " + palabra);
+                break;
+            }
+
+            System.out.print("escribi una letra: ");
+            String entrada = scanner.next();
+
+            char letra = entrada.charAt(0);
+            boolean acierto = false;
+
+            for (int i = 0; i < palabra.length(); i++) {
+                if (palabra.charAt(i) == letra) {
+                    descubierta[i] = letra;
+                    acierto = true;
+                }
+            }
+
+            if (acierto) {
+                System.out.println("la letra " + letra + " esta en la palabra\n");
+            } else {
+                errores++;
+                System.out.println("la letra " + letra + " no esta en la palabra\n");
+            }
+        }
+
+        if (errores >= erroresmax) {
+            System.out.println("\nno tenes mas intentos, la palabra era: " + palabra);
+        }
+
+        scanner.close();
+    }
+}
