ef25889405dece040ba0c5f47c520b769173b981
Juarez asheee <lucasjuar2008@gmail.com>
2026-06-17 21:55:10 -0300

    Ejercitacion7P1
    Punto1 de la ejercitacion7

---
+++ Punto1/Producto.java
@@ -0,0 +1,13 @@
+public class Producto {
+
+    public String nombre;
+    public String codigo;
+    public int precio;
+
+    public Producto(String nombre, String codigo, int precio) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.precio = precio;
+
+    }
+}
