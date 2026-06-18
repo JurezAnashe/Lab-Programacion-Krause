63425ced0d599ef0cb5a1a1b959b1a3ca63762d8
Juarez asheee <lucasjuar2008@gmail.com>
2026-06-18 20:29:24 -0300

    P2 Ejercitacion7

---
+++ P2 Ejercitacion7/Libro.java
@@ -0,0 +1,11 @@
+public class Libro {
+    public String titulo;
+    public String isbn;
+    public int cantpag;
+
+    public Libro(String titulo, String isbn, int cantpag) {
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.cantpag = cantpag;
+    }
+}
